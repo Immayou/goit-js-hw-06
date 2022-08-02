be2b7156e6934833df609ba6b94df3edc80e4b16
Yuliya Zozulya <djulia@ukr.net>
2022-08-02 15:10:26 +0300

    Виправляє домашнє завдання 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,16 @@
-const listCounter = document.querySelectorAll('.item')
-console.log(listCounter.length)
+const list = document.querySelector('#categories')
+const listItemsCounter = list.children.length;
+console.log(`Number of categories: ${listItemsCounter}`)
 
-const titleName = document.textContent('h2')
+const titleNames = document.querySelectorAll('h2')
+titleNames.forEach(function (title) {
+    console.log(`Category: ${title} 
+Elements:`)})
+for (let i = 0; i < titleNames.length; i += 1) { 
+    const title = titleNames[i].textContent; 
+    
+    console.log(`Category: ${title} 
+Elements:`);
+  }
+const items = document.querySelectorAll('.item > ul')
+console.log(items)
