4e6439345e7de1168bf75da63ca4260c2e8f9501
Yuliya Zozulya <djulia@ukr.net>
2022-08-05 01:24:49 +0300

    Доповнює вирішення 4 завдання

--- js/task-04.js
+++ js/task-04.js
@@ -1,10 +1,28 @@
-let counterValue = document.querySelector("#value");
-const onButtonIncrementClick = document.querySelector('[data-action = "increment"]')
-const onButtonDecrementClick = document.querySelector('[data-action = "decrement"]')
+const counterValue = document.querySelector("#value");
+let valueEl = counterValue.textContent;
+const onIncrementButtonClick = document.querySelector('[data-action = "increment"]')
+const onDecrementButtonClick = document.querySelector('[data-action = "decrement"]')
+console.log(valueEl)
 
 function counter () {
-counterValue = 0;
-onButtonIncrementClick.addEventListener('click', () => {
-counterValue += 1
-})
+onIncrementButtonClick.addEventListener('click', pushIncrement)
+onDecrementButtonClick.addEventListener('click', pushDecrement)
 }
+
+function pushIncrement () {
+    valueEl += 1
+    console.log("+")
+}
+
+function pushDecrement () {
+    valueEl -= 1
+    console.log("-")
+}
+
+// function buttonCounterClick (event) {
+//     let valueEl = counterValue.textContent;
+//     counterValue.textContent = parseInt(valueEl) + 1;
+//     }
+
+// onIncrementButtonClick.addEventListener('click', buttonCounterClick)
+// onDecrementButtonClick.addEventListener('click', buttonCounterClick)
