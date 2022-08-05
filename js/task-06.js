d45c25e7f789dc35290ee5668a1477a31ba8476a
Yuliya Zozulya <djulia@ukr.net>
2022-08-05 23:35:36 +0300

    Додає вирішення 6 задачі

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,20 @@
 const inputBlock = document.querySelector("#validation-input")
 const inputValidSymbolLength = inputBlock.getAttribute("data-length")
+console.log(inputValidSymbolLength)
 
 inputBlock.addEventListener("blur", onValidationCheckInput)
 
 function onValidationCheckInput (event) {
-    event.currentTarget.value.length === inputValidSymbolLength ?
-    
+
+    if(event.currentTarget.value.length === parseInt(inputValidSymbolLength)) {
+    inputBlock.classList.contains('invalid') ?
+    inputBlock.classList.replace('invalid', 'valid')
+    : inputBlock.classList.add('valid')
+    } else {
+    inputBlock.classList.contains('valid') ?
+    inputBlock.classList.replace('valid', 'invalid')
+    : inputBlock.classList.add('invalid')
+    }   
+
 }
 
