39d79301b94d4d8bfc0b4b17cfcb9b300ea9039c
Yuliya Zozulya <djulia@ukr.net>
2022-08-06 22:34:56 +0300

    Завершує виконання 8 завдання

--- js/task-08.js
+++ js/task-08.js
@@ -1,30 +1,20 @@
-const formEl = document.querySelector('.login-form')
+const form = document.querySelector('.login-form')
 
-formEl.addEventListener('submit', onFormSubmit)
+form.addEventListener('submit', onFormSubmit)
 
-function onFormSubmit (event) {
+function onFormSubmit(event) {
     event.preventDefault()
-
-    const formData = new FormData(event.currentTarget)
-    let valueArray = {};
-    formData.forEach((value, key) => {
-    if(event.currentTarget.elements.email.value === "" || event.currentTarget.elements.password.value === "") {
-    const message = 'Будь ласка, заповніть усі поля форми!'
-    alert(message)
-    } else {
-    valueArray = `${key}: ${value}`
-   
-    console.log(valueArray)
-    } 
-    // if(event.currentTarget.elements.email.value !== "" && event.currentTarget.elements.password.value !== "") {
-    // event.currentTarget.reset()
-    // } 
     
-    })
-}
-// console.log(`Login: ${event.currentTarget.elements.email.value}, Password: ${event.currentTarget.elements.password.value}`)
+    const {
+        elements: { email, password },
+    } = event.currentTarget
 
-// якщо є незаповнене поле -  alert с предупреждением о том, что все поля должны быть заполнены.
-// якщо заповнено все поля и отправил форму, збери значения полів в обьект, де имя поля будет именем свойства, 
-// а значение поля - значением свойства. 
-// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+    if (email.value === '' || password.value === '') {
+        const message = 'Будь ласка, заповніть усі поля форми!'
+        return alert(message)
+    }
+    const inputDataFormArray = { Email: email.value, Password: password.value }
+    
+    console.log(inputDataFormArray)
+    event.currentTarget.reset()
+}
